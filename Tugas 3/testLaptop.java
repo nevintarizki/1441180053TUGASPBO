2a5c30f115529c7b4e9df1b4fcbb69bed8cc8705
nevintarizki <nevinrizki01@gmail.com>
2015-04-15 10:56:01 +0700

    baruu
    yang kemaren ada erornya

--- Tugas 3/testLaptop.java
+++ Tugas 3/testLaptop.java
@@ -2,10 +2,13 @@ public class testLaptop {
 	public static void main(String[] args) {
 		Laptop laptop1 = new Laptop();
 		Laptop laptop2 = new Laptop("Acer","Hitam");
-		Laptop laptop3 =new Laptop("Axioo","Putih","TidakTersambung");
+		Laptop laptop3 = new Laptop("Axioo","Putih","TidakTersambung");
 		Laptop laptop4 = new Laptop("Vaioo","Biru","TidakTersambung",60);
 
-		System.out.println("Merk: "+laptop1.getMerk+" Warna: "+laptop1.getWarna+laptop1.indikatorWifi+laptop1.indikatorBaterai+"");
+		System.out.println("Merk: "+ laptop1.getMerk()      + "Warna: " + laptop1.getWarna() + "Wifi: " + laptop1.getWifi() + "Baterai: " + laptop1.getBaterai());
+		System.out.println("Merk: "+ laptop2.getMerk() + "Warna: " + laptop2.getWarna() + "Wifi: " + laptop2.getWifi() + "Baterai: " + laptop2.getBaterai());
+		System.out.println("Merk: "+ laptop3.getMerk() + "Warna: " + laptop3.getWarna() + "Wifi: " + laptop3.getWifi() + "Baterai: " + laptop3.getBaterai());
+		System.out.println("Merk: "+ laptop4.getMerk() + "Warna: " + laptop4.getWarna() + "Wifi: " + laptop4.getWifi() + "Baterai: " + laptop4.getBaterai());
 
 	}
 }
